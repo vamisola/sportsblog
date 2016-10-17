0101e9fc561dd4316a1e97c147d498efc82516a4
Vina Compson <vv.compson@icloud.com>
2016-10-17 02:44:42 +0000

    first commit for Sports Blog app

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+var Article = require('../models/article.js');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    Article.getArticles(function(err, articles){
+        if(err){
+            res.send(err);
+        }else{
+            res.render('index', { 
+                title: 'Express',
+                articles: articles });
+        }
+    });
+  
+});
+
+module.exports = router;

4cf649b4119c1e4b0301fd856ce4f6e4942eb684
Vina Compson <vv.compson@icloud.com>
2016-10-17 03:46:59 +0000

    add show article and all articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var Article = require("../models/article.js");
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('articles', { title: 'All Articles' });
+    Article.getArticles(function(err, articles){
+        if(err){
+            res.send(err);
+        }else{
+            res.render('articles', { 
+                title: 'All Articles',
+                articles: articles });
+        }
+    });
+  
 });
 
 router.get('/show/:id', function(req, res, next) {
-  res.render('article');
+  Article.getArticleById([req.params.id], function(err, article){
+    if(err){
+            res.send(err);
+        }else{
+            res.render('article', { 
+                article: article });
+        }
+  });
 });
 
 router.get('/category/:category_id', function(req, res, next) {
